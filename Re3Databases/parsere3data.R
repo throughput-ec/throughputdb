b8159feafa858e9321012a955adda3b8d5289213
Simon <simon.j.goring@gmail.com>
2019-06-10 21:44:09 -0700

    Cleaning the R runs.\nShould have fixed the issue with multiple links to common schemas.

--- Re3Databases/parsere3data.R
+++ Re3Databases/parsere3data.R
@@ -14,8 +14,6 @@ repo <- list()
 
 call_repo <- function(x) {
 
-  Sys.sleep(5)
-
   small_repos <- xml2::read_xml(paste0(re3api_short, all_repos[[x]]$id)) %>%
     xml2::as_list() %>%
     unlist(recursive = FALSE)
@@ -39,11 +37,12 @@ call_repo <- function(x) {
 }
 
 for(i in 1:length(all_repos)) {
+  cat("Running ", i, "  of ", length(all_repos), "\n")
   test_run <- try(call_repo(i))
   if ("try-error" %in% class(test_run)) {
-    repo[[i]] <- data.frame(name = unlist(all_repos[[x]]$name),
-                      url = unlist(all_repos[[x]]$id),
-                      stringsAsFactors = FALSE)
+    repo[[i]] <- data.frame(name = unlist(all_repos[[i]]$name),
+                            url = unlist(all_repos[[i]]$id),
+                            stringsAsFactors = FALSE)
   } else {
     repo[[i]] <- test_run
   }
@@ -53,10 +52,10 @@ for(i in 1:length(all_repos)) {
 
 all_repo <- repo %>% bind_rows()
 
-con_list <- jsonlite::readJSON('../connect_remote.json')
+con_list <- jsonlite::fromJSON('../connect_remote.json')
 
 con <- neo4j_api$new(
-  url = paste0("http://localhost:", con_list$port),
+  url = paste0("http://localhost:", con_list$r_port),
   user = con_list$user,
   password = con_list$password
 )
