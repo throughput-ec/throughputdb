d42b038b5a04edb869785f3f884d4544617b0735
Simon <simon.j.goring@gmail.com>
2019-06-13 10:28:07 -0700

    Working on the GitHub scraper. Fixed the token for the repository, changed the way we do the code search so that we are searching for code specifically, not repositories at the high level.

--- github_scraper/github_scrape.py
+++ github_scraper/github_scrape.py
@@ -4,12 +4,14 @@
 Linking to github repositories to find all repositories that contain code
 related to the Re3Data databases.
 
+This code hits the abuse detection mechanism, even with the pausing.
 '''
 
 from github import Github
 from py2neo import Graph
 from py2neo.data import Node, Relationship
 import json
+import random
 import urllib
 import requests
 import re
@@ -22,16 +24,13 @@ graph = Graph(**data)
 
 tx = graph.begin()
 
-with open('gh.token') as f:
-    token = f.readlines()
-
 # The use of the >>>!!!<<< is used to show deprecation apparently.
 cypher = """MATCH (ob:OBJECT)-[:isType]-(:TYPE {type:'schema:DataCatalog'})
             WHERE NOT ob.description CONTAINS '>>>!!!<<<'
             RETURN ob"""
 dbs = graph.run(cypher).data()
 
-with open(gh.token) as f:
+with open('gh.token') as f:
     token = json.load(f)
 
 g = Github(**token)
@@ -39,17 +38,44 @@ g = Github(**token)
 gitadd = open('cql/github_linker.cql', mode="r")
 git_cql = gitadd.read()
 
+random.shuffle(dbs)
+
 for db in dbs:
-    time.sleep(8)
+    time.sleep(10)
     print("Running graphs for", db['ob']['name'])
+    repositories = []
+    searchString = '"' + db['ob']['name'] + '" ' + db['ob']['url'] + ' in:file'
+    rate_limit = g.get_rate_limit()
+    print("   There are " + str(rate_limit.search.remaining) + " calls to GitHub remaining.")
+    if rate_limit.search.remaining < 2:
+        print("   Rate limit reached, pausing for 10 seconds.")
+        time.sleep(10)
     try:
-        repositories = g.search_repositories(query=db['ob']['name'])
-        for rep in repositories:
-            rep_info = { 'ghid': rep.id, 'ghurl': rep.html_url, 'ghdescription': rep.description, 'ghname': rep.name , 'otid':db['ob']['id'] }
-            if rep_info['ghdescription'] is None:
-                rep_info['ghdescription'] = ''
-            test = graph.run("MATCH p=(ob1:OBJECT)-[]-(an:ANNOTATION)-[]-(ob2:OBJECT) WHERE ob1.id = {ghid} AND ob2.id = {otid} RETURN an", rep_info)
-            if len(test.data()) == 0:
-                graph.run(git_cql, rep_info)
+        content_files = g.search_code(query=searchString)
     except:
-        continue
+        print("Sleeping for 30 seconds")
+        time.sleep(30)
+        g = Github(**token)
+        next
+    print("   Returning " + str(content_files.totalCount) + " results.")
+    for content in content_files:
+        time.sleep(2)
+        print("   There are " + str(rate_limit.search.remaining) + " calls to GitHub remaining.")
+        repo = content.repository
+        repElem = { 'ghid': repo.id,
+                    'ghurl': repo.html_url,
+                    'ghdescription': repo.description,
+                    'ghname': repo.full_name ,
+                    'otid':db['ob']['id'] }
+        repositories.append(repElem)
+
+    repositories = list({v['ghid']:v for v in repositories}.values())
+    print("   Pushing information for " + str(len(repositories)) + " public repositories.")
+
+    for rep in repositories:
+        if rep['ghdescription'] is None:
+            rep['ghdescription'] = ''
+        test = graph.run("MATCH p=(ob1:OBJECT)-[]-(an:ANNOTATION)-[]-(ob2:OBJECT) WHERE ob1.id = {ghid} AND ob2.id = {otid} RETURN an", rep)
+        if len(test.data()) == 0:
+            print("    * Adding repository to the graph.")
+            graph.run(git_cql, rep)
