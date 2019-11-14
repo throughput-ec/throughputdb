8a8becb79767cf9b06bb4f83e6fd2466423ef1d0
Simon <simon.j.goring@gmail.com>
2019-11-14 13:56:55 -0800

    Added a term to `call_repo.R` and added an element to pull the language into the `data.frame` output.

--- Re3Databases/R/call_repo.R
+++ Re3Databases/R/call_repo.R
@@ -1,14 +1,24 @@
 #' @title call_repo
 #' @description Calls a re3data database using the short identifier.
-#' @params x The index of the
-call_repo <- function(x) {
+#' @param x The repository identifier in Re3Data
+#' @param re3api_short The beta API path to extract information about a re3data product.
+
+call_repo <- function(x, re3api_short) {
 
   small_repos <- xml2::read_xml(paste0(re3api_short, x)) %>%
     xml2::as_list() %>%
     unlist(recursive = FALSE)
 
   all_kw <- names(small_repos$re3data.repository) == "keyword"
-  keywords <- paste0(unlist(small_repos$re3data.repository[all_kw]), collapse = ",")
+  keywords <- small_repos$re3data.repository[all_kw] %>%
+    unlist %>%
+    tolower %>%
+    paste0(collapse = ",")
+
+  lang_names <- names(small_repos$re3data.repository) == "repositoryLanguage"
+  lang <- small_repos$re3data.repository[which(lang_names)]
+  lang <- lang %>% unlist %>% paste0(collapse=",")
+
   description <- unlist(small_repos$re3data.repository$description)
 
   if (is.null(description)) {
@@ -19,6 +29,7 @@ call_repo <- function(x) {
                    url = unlist(small_repos$re3data.repository$repositoryURL),
                    keywords = keywords,
                    id = unlist(small_repos$re3data.repository$re3data.orgIdentifier),
+                   lang = lang,
                    description = description,
                    stringsAsFactors = FALSE))
 
