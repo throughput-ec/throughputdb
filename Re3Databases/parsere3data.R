48cf47f5721088cfadec6d34d3449b07531dbb9e
Simon <simon.j.goring@gmail.com>
2020-02-20 16:37:48 -0800

    Updated language management for the R Re3data files.

--- Re3Databases/parsere3data.R
+++ Re3Databases/parsere3data.R
@@ -4,11 +4,12 @@ library(xml2)
 library(dplyr)
 library(readr)
 library(purrr)
+library(cld3)
 
 re3api_full <- "https://www.re3data.org/api/v1/repositories"
 re3api_short <- "https://www.re3data.org/api/beta/repository/"
 
-cat("Calling re3data for full repo list")
+cat("Calling re3data for full repo list\n")
 
 all_repos <- xml2::read_xml(re3api_full) %>%
   xml2::as_list() %>%
@@ -19,16 +20,17 @@ all_repos <- xml2::read_xml(re3api_full) %>%
   bind_rows()
 
 source("R/call_repo.R")
+
 import <- readr::read_file("cql/linkdbs.cql")
 
 failures <- list()
 
-con_list <- jsonlite::fromJSON('../connect_remote.json')
+con_list <- jsonlite::fromJSON('../connect_remote.json')[2,]
 
 con <- neo4j_api$new(
-  url = paste0("http://localhost:", con_list$r_port),
-  user = con_list$user,
-  password = con_list$password
+  url = paste0(con_list$host, ":", con_list$r_port),
+  user = unlist(con_list$user),
+  password = unlist(con_list$password)
 )
 
 for(i in 1:nrow(all_repos)) {
@@ -38,9 +40,9 @@ for(i in 1:nrow(all_repos)) {
     call_neo4j(con) %>%
     unlist()
 
-  if (has_match == 0) {
+  test_run <- try(call_repo(all_repos$id[i], re3api_short))
 
-    test_run <- try(call_repo(all_repos$id[i]))
+  if (has_match == 0) {
 
     if ("try-error" %in% class(test_run)) {
       failures[[length(failures) + 1 ]] <- data.frame(id = all_repos$id[i],
@@ -52,11 +54,29 @@ for(i in 1:nrow(all_repos)) {
               test_run$name,
               test_run$url,
               test_run$keywords,
-              gsub("'|\"", "", test_run$description)) %>% call_neo4j(con)
-
+              gsub("'|\"", "", test_run$description)) %>%
+        call_neo4j(con)
     }
   }
 
+  if (!"try-error" %in% class(test_run)) {
+    lang <- detect_language(test_run$description)
+
+    inset <- paste0("MATCH (ln:lng_LANGUAGE {subtag:'",lang,"'}) RETURN COUNT(ln)") %>%
+      call_neo4j(con) %>%
+      unlist()
+
+    if (inset > 0) {
+      paste0("MATCH (o:OBJECT {id:'",all_repos$id[i],"'})
+       MATCH (ln:lng_LANGUAGE {subtag:'",lang,"'})
+       WITH o, ln
+       MERGE (o)-[:hasLanguage]-(ln);") %>%
+           call_neo4j(con)
+      cat(paste0(" Added language '", lang, "' to ", test_run$name, "\n"))
+    }
+  }
 }
 
-failures %>% bind_rows() %>% readr::write_csv("failed_archives.csv")
+failures %>%
+  bind_rows() %>%
+  readr::write_csv("failed_archives.csv")
