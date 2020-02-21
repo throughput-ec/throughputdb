77f8b98a54c2be851ea5f100e528d3b07fb566df
Simon <simon.j.goring@gmail.com>
2020-02-20 16:39:41 -0800

    Fixed the way move_keywords connects to the DB.

--- Re3Databases/move_keywords.R
+++ Re3Databases/move_keywords.R
@@ -6,12 +6,12 @@ library(readr)
 library(purrr)
 library(cld3)
 
-con_list <- jsonlite::fromJSON('../connect_remote.json')
+con_list <- jsonlite::fromJSON('../connect_remote.json')[2,]
 
 con <- neo4j_api$new(
-  url = 'localhost:7474',
-  user = con_list$user,
-  password = 'test'
+  url = paste0(con_list$host, ":", con_list$r_port),
+  user = unlist(con_list$user),
+  password = unlist(con_list$password)
 )
 
 query <- 'MATCH (n:OBJECT)-[:isType]-(:TYPE {type:"schema:DataCatalog"})
