e2c63e3967c8e17f54ce4d0d7e42d4e612aac874
Simon <simon.j.goring@gmail.com>
2020-02-24 21:39:23 -0800

    Cleaning up the award stuff. Updated the parameterized query to run as LOAD CSV.

---
+++ nsf_awards/nsf_awards.R
@@ -0,0 +1,79 @@
+library(purrr)
+library(dplyr)
+library(jsonlite)
+library(neo4r)
+library(httr)
+library(xml2)
+
+fileloc <- paste0('file://////', getwd())
+
+source('R/bind_to_file.R')
+query <- readr::read_file('cql/parameterized.cql')
+con_list <- jsonlite::fromJSON('../connect_remote.json')[2,]
+
+con <- neo4j_api$new(
+  url = paste0(con_list$host, ":", con_list$r_port),
+  user = unlist(con_list$user),
+  password = unlist(con_list$password)
+)
+
+nsfawards <- list.files('data/input/awards', full.names = TRUE)
+
+for (i in 1:length(nsfawards)) {
+  cat(sprintf("\n***\nUnzipping %s\n", nsfawards[i]))
+  unzip(nsfawards[i], exdir = 'data/input/awards/unzip')
+  unzippers <- list.files('data/input/awards/unzip', full.names = TRUE)
+
+  big_table <- list()
+  cat(sprintf("\nBinding %d files\n", length(unzippers)))
+
+  to_df <- function(x){
+    df <- bind_to_file(x)
+
+    if('try-error' %in% class(df)) {
+      return(df)
+    }
+
+    df[is.na(df)] <- "None"
+    colnames(df) <- gsub("\\.", "", colnames(df))
+    return(df)
+  }
+
+  for(j in 1:length(unzippers)) {
+    run_df <- try(to_df(unzippers[j]))
+    if('try-error' %in% class(run_df)) {
+        big_table[[j]] <- NULL
+        file.copy(unzippers[j], to = 'data/output/errors/')
+    } else {
+        big_table[[j]] <- run_df
+    }
+
+    cat('.')
+    if (j%%50 == 0) {
+      cat('', j, '\n')
+    }
+  }
+
+  cat('\n')
+  
+  datecheck <- function(x) {
+    res <- (is.null(x) | is.na(x) | x %in% c("NA", "None"))
+    x[res] <- "0/0/0"
+    return(x)
+  }
+
+  years <- big_table  %>%
+    bind_rows() %>%
+    mutate_at(vars(contains("Date")), datecheck) %>%
+    readr::write_csv(path='/var/lib/neo4j/import/awards.csv')
+
+  cat(sprintf('\nRunning query for the %dth file from NSF: %s\n',
+              i, nsfawards[i]))
+
+  cat(sprintf('\nA total of %d award files to be run against.\n*****\n',
+              length(unzippers)))
+  query <- readr::read_file('cql/parameterized.cql')
+  call_neo4j(con, query=query)
+
+  unlink(unzippers)
+}
