6142c3fdbd7b4b3bf1558adeb49845638d194bf9
Simon <simon.j.goring@gmail.com>
2020-03-16 13:35:27 -0700

    Changing dockrun to work with development and production.

--- dockrun.sh
+++ dockrun.sh
@@ -4,12 +4,12 @@ docker run \
     --name testneo4j \
     -p7474:7474 -p7687:7687 \
     -d \
-    -v $HOME/neo4j/data:/data \
-    -v $HOME/neo4j/logs:/logs \
-    -v $HOME/neo4j/import:/var/lib/neo4j/import \
-    -v $HOME/neo4j/plugins:/plugins \
+    -v $NJHOME/data:/data \
+    -v $NJHOME/logs:/logs \
+    -v $NJHOME/import:/var/lib/neo4j/import \
+    -v $NJHOME/plugins:/plugins \
     --ulimit=nofile=40000:40000 \
-    --env NEO4J_AUTH=neo4j/test \
+    --env NEO4J_AUTH=neo4j/$NJPASS \
     neo4j:latest 2> /dev/null || \
     docker start testneo4j
 
