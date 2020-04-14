4ea068d82c78f6da2008f422befb09e1744da124
Simon Goring <goring@wisc.edu>
2020-04-14 12:20:09 -0700

    Added more documentation to `parsere3.py`. Part of the goal is to improve discoverability from the `github_scraper`.

--- Re3Databases/parsere3.py
+++ Re3Databases/parsere3.py
@@ -1,5 +1,10 @@
 def parsere3(uri):
     """Parse Re3Data from XML to Python dictionary.
+    This script uses the API for the Registry of Research Data Repositories
+    at http://re3data.org/ to pull information about major data repositories
+    and then parses it into a usable Python format, from XML.
+
+    API Documentation for Re3Data can be found at: http://re3data.org/api/doc
 
     Parameters
     ----------
