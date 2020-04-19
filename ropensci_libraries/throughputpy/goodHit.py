a4aac7b9be9b04d760a28b5c2b53083248aec2cd
Simon Goring <goring@wisc.edu>
2020-04-18 17:04:21 -0700

    Moving `def`s into a clean folder. Trying to make the main code a bunch cleaner.

---
+++ ropensci_libraries/throughputpy/goodHit.py
@@ -0,0 +1,24 @@
+from re import match
+
+
+def goodHit(query, text):
+    """Check for expected query call in file content.
+
+    Parameters
+    ----------
+    query : str
+        Text string passed to the original GitHub code search query.
+    text : list
+        The File contents, including highlighted fragments.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
+    strings = query.split(" ")
+    matcher = strings[0] + r'\(([^\)]*' + strings[1][:-1] + ')'
+    check = list(map(lambda x: match(matcher, x.get('fragment')), text))
+    output = not(all(matches is None for matches in check))
+    return output
