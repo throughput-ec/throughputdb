a53b02f241f8d2a2186b8a77a55e72906b9f8578
Simon Goring <goring@wisc.edu>
2020-04-20 15:59:02 -0700

    Added missed-match logging to `goodHit`. Also increased the number of hits for matches using multiple regex calls.

--- ropensci_libraries/throughputpy/goodHit.py
+++ ropensci_libraries/throughputpy/goodHit.py
@@ -1,4 +1,5 @@
-from re import match
+from re import search
+import json
 
 
 def goodHit(query, text):
@@ -17,8 +18,26 @@ def goodHit(query, text):
         Description of returned object.
 
     """
+
     strings = query.split(" ")
-    matcher = strings[0] + r'\(([^\)]*' + strings[1] + ')'
-    check = list(map(lambda x: match(matcher, x.get('fragment')), text))
+    matchlib = ".*" + strings[0] + r'\(([^\)]*' + strings[1] + ')'
+    matchcal = strings[1] + r'\:\:'
+    matchvig = r".*VignetteDepends\{([^\}])*" + strings[1]
+    checklib = list(map(lambda x: search(matchlib, x.get('fragment')), text))
+    checkcal = list(map(lambda x: search(matchcal, x.get('fragment')), text))
+    checkvig = list(map(lambda x: search(matchvig, x.get('fragment')), text))
+    check = checkcal + checklib + checkvig
     output = not(all(matches is None for matches in check))
+    if output is not True:
+        f = open("fail_log.txt", "a")
+        textdump = {'query': query,
+                    'text': list(map(lambda x: x.get('fragment'), text))}
+        f.write(json.dumps(textdump) + "\n")
+        f.close()
+    else:
+        f = open("pass_log.txt", "a")
+        textdump = {'query': query,
+                    'text': list(map(lambda x: x.get('fragment'), text))}
+        f.write(json.dumps(textdump) + "\n")
+        f.close()
     return output
