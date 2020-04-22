8cd09383a189fa8362ef81ddbd8190e7a71c3507
Simon Goring <goring@wisc.edu>
2020-04-21 21:29:14 -0700

    Starting translation of nsf awards files.

---
+++ nsf_awards/nsf_awards.py
@@ -0,0 +1,23 @@
+import xmltotext
+import json
+from py2neo import Graph
+
+with open('../.gitignore') as gi:
+    good = False
+    # This simply checks to see if you have a connection string in your repo.
+    # I use `strip` to remove whitespace/newlines.
+    for line in gi:
+        if line.strip() == "connect_remote.json":
+            good = True
+            break
+
+if good is False:
+    print("The connect_remote.json file is not in your .gitignore file. \
+           Please add it!")
+
+with open('../connect_remote.json') as f:
+    data = json.load(f)
+
+graph = Graph(**data[1])
+
+tx = graph.begin()
