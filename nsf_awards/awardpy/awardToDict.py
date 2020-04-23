7a4bfa2f3e88cedc1a386f813b2a86b020968a30
Simon Goring <goring@wisc.edu>
2020-04-23 12:46:53 -0700

    Moved functions for Awards into awardpy.

--- nsf_awards/awardpy/awardToDict.py
+++ nsf_awards/awardpy/awardToDict.py
@@ -8,8 +8,10 @@ def awardToDict(award):
                  'AwardAmount': tags.get('AwardAmount'),
                  'ARRAAmount': tags.get('ARRAAmount'),
                  'OrganizationCode': tags.get('Organization').get('Code'),
-                 'OrganizationDirectorate': tags.get('Organization').get('Directorate').get('LongName'),
-                 'OrganizationDivision': tags.get('Organization').get('Division').get('LongName'),
+                 'OrganizationDirectorate': tags.get('Organization').get(
+                                            'Directorate').get('LongName'),
+                 'OrganizationDivision': tags.get('Organization').get(
+                                            'Division').get('LongName'),
                  'MinAmdLetterDate': tags.get('MinAmdLetterDate'),
                  'MaxAmdLetterDate': tags.get('MaxAmdLetterDate'),
                  'AwardInstrument': tags.get('AwardInstrument').get('Value'),
@@ -17,45 +19,51 @@ def awardToDict(award):
                  'AwardExpirationDate': tags.get('AwardExpirationDate'),
                  'AbstractNarration': clean(tags.get('AbstractNarration'),
                                             lower=False, no_line_breaks=True)
-                }
-
+                 }
+    if tags.get('Investigator') is None:
+        awardDict['Investigator'] = [{'FirstName': None,
+                                      'LastName': None,
+                                      'EmailAddress': None,
+                                      'StartDate': None,
+                                      'EndDate': None,
+                                      'RoleCode': None}]
+    else:
+        if type(tags.get('Investigator')) is list:
+            awardDict['Investigator'] = list(map(lambda x: dict(x),
+                                                 tags.get('Investigator')))
+        else:
+            awardDict['Investigator'] = [dict(tags.get('Investigator'))]
     if tags.get('Institution') is None:
-        awardDict['InstitutionStateName'] = None
-        awardDict['InstitutionStateCode'] = None
-        awardDict['InstitutionCountryName'] = None
-        awardDict['InstitutionName'] = None
-        awardDict['InstitutionPhoneNumber'] = None
-        awardDict['InstitutionCityName'] = None
-        awardDict['InstitutionStreetAddress'] = None
-        awardDict['InstitutionZipCode'] = None
+        awardDict['Institution'] = [{'Name': None,
+                                     'CityName': None,
+                                     'ZipCode': None,
+                                     'PhoneNumber': None,
+                                     'StreetAddress': None,
+                                     'CountryName': None,
+                                     'StateName': None,
+                                     'StateCode': None}]
     else:
-        awardDict['InstitutionStateName'] = tags.get('Institution').get('StateName')
-        awardDict['InstitutionStateCode'] = tags.get('Institution').get('StateCode')
-        awardDict['InstitutionCountryName'] = tags.get('Institution').get('CountryName')
-        awardDict['InstitutionName'] = tags.get('Institution').get('Name')
-        awardDict['InstitutionPhoneNumber'] = tags.get('Institution').get('PhoneNumber')
-        awardDict['InstitutionCityName'] = tags.get('Institution').get('CityName')
-        awardDict['InstitutionStreetAddress'] = tags.get('Institution').get('StreetAddress')
-        awardDict['InstitutionZipCode'] = tags.get('Institution').get('ZipCode')
-
-    if type(tags.get('ProgramElement')) is list:
-        awardDict['ProgramElementCode'] = list(map(lambda x: x.get('Code'), tags.get('ProgramElement')))
-        awardDict['ProgramElementText'] = list(map(lambda x: x.get('Text'), tags.get('ProgramElement')))
-    else:
-        if type(tags.get('ProgramElement')) is None:
-            awardDict['ProgramElementCode'] = None
-            awardDict['ProgramElementText'] = None
+        if type(tags.get('Institution')) is list:
+            awardDict['Institution'] = list(map(lambda x: dict(x),
+                                            tags.get('Institution')))
         else:
-            awardDict['ProgramElementCode'] = tags.get('ProgramElement').get('Code')
-            awardDict['ProgramElementText'] = tags.get('ProgramElement').get('Text')
-    if type(tags.get('ProgramReference')) is list:
-        awardDict['ProgramReferenceCode'] = list(map(lambda x: x.get('Code'), tags.get('ProgramReference')))
-        awardDict['ProgramReferenceText'] = list(map(lambda x: x.get('Text'), tags.get('ProgramReference')))
+            awardDict['Institution'] = [dict(tags.get('Institution'))]
+    if tags.get('ProgramElement') is None:
+        awardDict['ProgramElement'] = {'Code': None,
+                                       'Text': None}
     else:
-        if type(tags.get('ProgramElement')) is None:
-            awardDict['ProgramReferenceCode'] = None
-            awardDict['ProgramReferenceText'] = None
+        if type(tags.get('ProgramElement')) is list:
+            awardDict['ProgramElement'] = list(
+                map(lambda x: dict(x), tags.get('ProgramElement')))
         else:
-            awardDict['ProgramReferenceCode'] = tags.get('ProgramReference').get('Code')
-            awardDict['ProgramReferenceText'] = tags.get('ProgramReference').get('Text')
+            awardDict['ProgramElement'] = [dict(tags.get('ProgramElement'))]
+    if tags.get('ProgramReference') is None:
+        awardDict['ProgramReference'] = {'Code': None,
+                                         'Text': None}
+    else:
+        if type(tags.get('ProgramReference')) is list:
+            awardDict['ProgramReference'] = list(
+                map(lambda x: dict(x), tags.get('ProgramReference')))
+        else:
+            awardDict['ProgramReference'] = [dict(tags.get('ProgramReference'))]
     return awardDict
--- nsf_awards/awardpy/awardUnZip.py
+++ nsf_awards/awardpy/awardUnZip.py
@@ -2,13 +2,24 @@ import tempfile
 from zipfile import ZipFile
 from os import listdir
 import xmltodict
+import sys
+from awardpy.awardToDict import awardToDict
+from cleantext import clean
 
 
 def awardUnZip(file):
     tmpdir = tempfile.TemporaryDirectory()
-    ZipFile(file).extractall(path = tmpdir.name)
+    ZipFile(file).extractall(path=tmpdir.name)
     awards = []
     for i in listdir(tmpdir.name):
-        with open(tmpdir.name + '/' + i) as xml:
-            awards.append(xmltodict.parse(xml.read()))
-    return(awards)
+        with open(tmpdir.name + '/' + i, encoding="ISO-8859-1") as xml:
+            try:
+                awards.append(xmltodict.parse(clean(xml.read(), lower=False)))
+            except Exception:
+                with open('./xml_logfile.log', 'a') as logger:
+                    logger.write('Failed to open ' + tmpdir.name + '/' + i
+                                 + ': ' + str(sys.exc_info()[0]) + '\n')
+    awardDict = []
+    for i in awards:
+        awardDict.append(awardToDict(i))
+    return(awardDict)
